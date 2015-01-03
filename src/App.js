438b60c698120ed74b0137116c0d61a2d7aa3210
Marian Sievers <marian.sievers@gmail.com>
2015-01-03 15:58:49 +0100

    Move app-code to seperat file

---
+++ src/App.js
@@ -0,0 +1,39 @@
+var koa = require("koa");
+
+var app = koa();
+
+exports.App = {
+    init: function () {
+        console.log("Init the app");
+
+        // x-response-time
+        app.use(function *(next) {
+            var start = new Date();
+            yield next;
+            var ms = new Date() - start;
+            this.set('X-Response-Time', ms + 'ms');
+        });
+
+        // logger
+        app.use(function *(next) {
+            var start = new Date();
+            yield next;
+            var ms = new Date() - start;
+            console.log('%s %s - %s ms', this.method, this.url, ms);
+        });
+
+        // ----- response -----
+
+        // static html
+
+        // assets
+
+        // api
+        app.use(function *() {
+            this.body = "This is the KommunalCrowd API";
+        });
+
+        // app start
+        app.listen(3000);
+    }
+};
