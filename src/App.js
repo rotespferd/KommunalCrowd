60dbf976f3b63b45e7cc8d96164dc0eb869e5506
Marian Sievers <marian.sievers@gmail.com>
2015-01-04 22:18:57 +0100

    Add logger for xResponseTime

--- src/App.js
+++ src/App.js
@@ -9,12 +9,7 @@ exports.App = {
         console.log("Init the app");
 
         // x-response-time
-        app.use(function *(next) {
-            var start = new Date();
-            yield next;
-            var ms = new Date() - start;
-            this.set('X-Response-Time', ms + 'ms');
-        });
+        app.use(Logger.xResponseTime);
 
         // logger
         app.use(Logger.executionTime);
--- src/middleware/Logger.js
+++ src/middleware/Logger.js
@@ -4,5 +4,11 @@ exports.Logger = {
         yield next;
         var ms = new Date() - start;
         console.log('%s %s - %s ms', this.method, this.url, ms);
+    },
+    xResponseTime: function *(next) {
+        var start = new Date();
+        yield next;
+        var ms = new Date() - start;
+        this.set('X-Response-Time', ms + 'ms');
     }
-}
+};
